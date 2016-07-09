2630de75c4bee28b35279b53285a2cc10f6989a3
chqmvum1 <yu_mi_hsnkn@yahoo.co.jp>
2016-07-09 10:13:50 +0900

    Add files via upload

--- lexer.rb
+++ lexer.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 #
 # 字句解析メソッドlexscan
 #
@@ -7,35 +8,52 @@ def lexscan(filename)
   
   tokens = []
   file = File.open(filename)
-  file.each_line do |line| #ファイルの内容を一行ずつ読み込む
+
+  
+  file.each_line do |line|
+    
     regexp = /([\w]+|\+|-|\*|\/|>=|<=|>|<|\(|\)|\[|\]|\{|\}|==|=|!=|,|;|&&|&|\|\|)/
     ary = line.split(regexp).select {|s| !s.empty? }
+    
     i = 0
     
     ary.each do |obj|
+      
       idx = line.index(ary[i])
       pos = "(l#{file.lineno}, #{idx})"
+      
       case obj
+          
       when  "int", "void", "if", "else", "do", "while", "for", "return"
         token = [obj.upcase.to_sym , ["#{obj}", "#{pos}"]]
+
       when  "\+", "-", "\*", "\/", ">", "<", ">=", "<=", "==", "=", "!=", "&&", "&", "\|\|"
         token = [ obj , ["#{obj}", "#{pos}"]]
+
       when "\["
         token = [:LBBRA, ["#{obj}", "#{pos}"]]
+
       when "\]"
         token = [:RBBRA, ["#{obj}", "#{pos}"]]
+
       when "\("
         token = [:LPAR, ["#{obj}", "#{pos}"]]
+
       when "\)"
         token = [:RPAR, ["#{obj}", "#{pos}"]]
+
       when "\{"
         token = [:LBRA, ["#{obj}", "#{pos}"]]
+
       when "\}"
         token = [:RBRA, ["#{obj}", "#{pos}"]]
+
       when ","
         token = [:COMMA, ["#{obj}", "#{pos}"]]
+
       when ";"
         token = [:SEMI, ["#{obj}", "#{pos}"]]
+
       else
         if /^[A-Za-z_]/ =~ obj
           token = [:IDENTIFIER, ["#{obj}", "#{pos}"]]
@@ -46,13 +64,19 @@ def lexscan(filename)
           token = "BLANK"
         end
       end
-      
+
       if token != "BLANK"
         tokens << token
       end
+
       i += 1
+
     end
   end
+  
   file.close
+
+  
   return tokens
+  
 end
